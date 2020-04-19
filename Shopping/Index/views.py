201762871b072e3eab153712ffcafb8e47f1ce14
IVYSS <61070075@kmitl.ac.th>
2020-04-19 19:31:54 +0700

    homepage login

---
+++ Shopping/Index/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def show(request):
+     return render(request, 'Index/index.html')
+
+def login(request):
+     return render(request, 'Index/login.html')
