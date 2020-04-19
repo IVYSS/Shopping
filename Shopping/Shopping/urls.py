e7b872b38157b3eb8cc45154660068b8ce401879
kuraigi <42972606+kuraigi@users.noreply.github.com>
2020-04-19 19:54:54 +0700

    แก้ url ให้ไปหน้าแรกเลยไม่ต้องใส่ index
    แก้ url ให้ไปหน้าแรกเลยไม่ต้องใส่ index

--- Shopping/Shopping/urls.py
+++ Shopping/Shopping/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', include('Profile.urls')),
-    path('index/', include('Index.urls')),
+    path('', include('Index.urls')),
 ]
