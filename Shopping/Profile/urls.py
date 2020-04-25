a58ef3c331d0821d490ec69513fb5ba3097e87d3
IVYSS <61070075@kmitl.ac.th>
2020-04-25 21:13:08 +0700

    ลบไฟล ละอัพใหม่

--- Shopping/Profile/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.showprofile, name='showprofile'),
-    path('profile_change/', views.profile_change, name='profile_change'),
-]
